649ed4ba9571c7733f4f6cda5e578d5d7d8aa012
martinman1991 <98027494+martinman1991@users.noreply.github.com>
2022-09-16 08:45:02 +0200

    Add files via upload

---
+++ hatalmasranovekvohonfoglalas/lista.py
@@ -0,0 +1,13 @@
+lista = ["seethe", "cope", "touch grass", 1337]
+print(lista)
+print(lista[0])
+print(len(lista))
+print(lista[3-1])
+print(lista[len(lista)-1])
+print(type(lista[len(lista)-1]))
+lista[2]="cement"
+print(lista)
+lista.append("sewer side")
+print(lista)
+lista.insert(1, "Kokkenballentorturen")
+print(lista)
