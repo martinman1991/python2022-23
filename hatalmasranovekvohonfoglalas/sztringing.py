9bbc9bff1268e61a1e6357ef80f82428038dad71
martinman1991 <98027494+martinman1991@users.noreply.github.com>
2022-10-07 13:20:13 +0200

    Add files via upload

---
+++ hatalmasranovekvohonfoglalas/sztringing.py
@@ -0,0 +1,10 @@
+nev = input("Aggy név pls: ")
+print("A " + nev + " nevet írtad be.")
+
+print("A {belsonev} nevet írtad be.".format(belsonev=nev))
+
+if len(nev)<5:
+    print("Your {belsonev} penis is very tiny.".format(belsonev=nev))
+elif len(nev)>=10:
+    print("Úristen very big {belsonev} penis.".format(belsonev=nev))
+while
