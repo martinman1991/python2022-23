0c7c747e75802c020df18a6ec531327f3f95bfc5
martinman1991 <98027494+martinman1991@users.noreply.github.com>
2022-10-10 13:16:16 +0200

    Add files via upload

---
+++ hatalmasranovekvohonfoglalas/gaykorlás.py
@@ -0,0 +1,11 @@
+beSzam=0
+
+while beSzam<2 or beSzam>5:
+    beSzam= int(input("ASDUHj meOF egy fsázmot 2 WAÉSD 5 közt: "))
+
+autok=[]
+for i in range(0,beSzam):
+    autok.append(input("RKQRKkkelkrekm a(z) " +str(i+1)+ ". atuhasomérkqwűáts: "))
+
+print(autok)
+
---
+++ hatalmasranovekvohonfoglalas/prokjewjtkt.py
@@ -0,0 +1,5 @@
+tag1 = int(input("Enter tag1:"))
+tag2 = int(input("Enter tag2:"))
+
+tag1 += tag2
+print(tag1)
