c1a1c05c9948f2879d2fa37b1fac4a0c463fabb3
martinman1991 <98027494+martinman1991@users.noreply.github.com>
2022-11-04 10:35:29 +0100

    Add files via upload

--- hatalmasranovekvohonfoglalas/matplotliberal.py
+++ hatalmasranovekvohonfoglalas/matplotliberal.py
@@ -5,8 +5,12 @@ print(matplotlib.__version__)
 import matplotlib.pyplot as plt
 import numpy as np
 
-xpoints = np.array([0, 6])
-ypoints = np.array([0, 250])
+xpoints = np.array([0, 16])
+ypoints = np.array([0, 25])
 
 plt.plot(xpoints, ypoints)
+plt.plot([10,20,12],[32,10,12])
 plt.show()
+
+x=[0,50,50,0,0]
+y=[0,0,50,50,0]
