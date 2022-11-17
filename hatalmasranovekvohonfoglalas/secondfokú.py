b51d3e3dcdb0f329b45e0512fab79688872f8a43
martinman1991 <98027494+martinman1991@users.noreply.github.com>
2022-11-17 13:19:21 +0100

    Add files via upload

--- hatalmasranovekvohonfoglalas/secondfokú.py
+++ hatalmasranovekvohonfoglalas/secondfokú.py
@@ -1,5 +1,21 @@
 import math
 
+def egyenlet(a,b,c):
+    szoveg="0 = "
+    if a!=0:
+        szoveg+=" + "+str(a)+"x²"
+    if b<0:
+        szoveg+=" + "+str(b)+"x"
+    elif b>0:
+        szoveg+=" + "+str(a)+"x"
+    if c>0:
+        szoveg+=" + "+str(c)
+    elif c<0:
+        szoveg+=" + "+str(c)
+
+    return szoveg
+    
+
 #a*x2+b*x+c
 
 a = int(input("a="))
@@ -10,15 +26,23 @@ c = int(input("c="))
 
 #(-b+-gyök(b2-4*a*c)/2a
 
+x1=""
+x2=""
 diszkriminans=b*b-4*a*c
 if diszkriminans<0:
     print("hülye fasz vagy")
 elif diszkriminans==0:
     megoldas=-b / (2*a)
-    print("hallod te kalányoshermész a megoldás: {}".format(megoldas))
+    x1=megoldas
+    x2=megoldas
+    print("a megoldás: {}".format(megoldas))
 else:
     x1=(-b+math.sqrt(diszkriminans)) / (2*a)
     x2=(-b-math.sqrt(diszkriminans)) / (2*a)
-#gyok=math.sqrt(1)
-#print(gyok)
+    print("2 megoldás: x1: {}, x2: {}".format(x1,x2))
 
+print(egyenlet(a,b,c))
+#a*(x-x1)*=x-x2)=0
+print(a)
+print(x1)
+print(x2)
