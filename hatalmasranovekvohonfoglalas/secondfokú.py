fc1ea412aefe4b3b497094c3280875aaf041091f
martinman1991 <98027494+martinman1991@users.noreply.github.com>
2022-11-18 13:20:31 +0100

    Add files via upload

--- hatalmasranovekvohonfoglalas/secondfokú.py
+++ hatalmasranovekvohonfoglalas/secondfokú.py
@@ -14,7 +14,37 @@ def egyenlet(a,b,c):
         szoveg+=" + "+str(c)
 
     return szoveg
-    
+
+def gyoktenyezosszorzat(a,x1,x2):
+     if x1=="":
+         return "No gyöktényezős alak???"
+    elif x1==x2:
+        if x1<0:
+            return str(a)+"(x + "+str(-)1*x1) +")²"
+        elif x1>0:
+            return str(a)+"(x - "+str(-)1*x1) +")²"
+        else:
+            return str(a)+"x²"
+    else:
+        if x1<0:
+            if x2<0:
+                return str(a)+"(x + "+str(-)1*x1) +")"str(a)+"(x + "+str(-)1*x2) +")"
+            elif x2>0:
+                return str(a)+"(x + "+str(-)1*x1) +")"str(a)+"(x - "+str(-)1*x2) +")"
+            else:
+                return str(a)+"(x + "+str(-)1*x1) +")x"
+        elif x1>0:
+            if x2<0:
+                return str(a)+"(x - "+str(-)1*x1) +")"str(a)+"(x + "+str(-)1*x2) +")"
+            elif x2>0:
+                return str(a)+"(x - "+str(-)1*x1) +")"str(a)+"(x - "+str(-)1*x2) +")"
+            else:
+                return str(a)+"(x - "+str(-)1*x1) +")x"
+        else:
+             if x2<0:
+                return str(a)+"(x + "+str(-)1*x1) +")x"
+            elif x2>0:
+                return str(a)+"(x + "+str(-)1*x1) +")x"
 
 #a*x2+b*x+c
 
@@ -46,3 +76,4 @@ print(egyenlet(a,b,c))
 print(a)
 print(x1)
 print(x2)
+print(gyoktenyzosszorzat(a,x1,x2))
---
+++ hatalmasranovekvohonfoglalas/szamBe.py
@@ -0,0 +1,9 @@
+#számbakérő modul
+#Többféel paraméterezéssel
+#2022.11.18 Balatincz Martin
+
+def szamBe(szoveg,tort=False,minimum=False,maximum=False):
+    print(szoveg)
+    print(tort)
+
+szamBe("Adgyá meg numbert ppölös: ",tort=True)
