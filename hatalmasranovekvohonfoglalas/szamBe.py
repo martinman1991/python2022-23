d63ab79040e9bd12d00094f3d0128a976cf99c7d
martinman1991 <98027494+martinman1991@users.noreply.github.com>
2022-11-29 14:49:13 +0100

    Add files via upload

--- hatalmasranovekvohonfoglalas/szamBe.py
+++ hatalmasranovekvohonfoglalas/szamBe.py
@@ -3,7 +3,18 @@
 #2022.11.18 Balatincz Martin
 
 def szamBe(szoveg,tort=False,minimum=False,maximum=False):
-    print(szoveg)
-    print(tort)
-
-szamBe("Adgyá meg numbert ppölös: ",tort=True)
+#    print(szoveg)
+#    print(tort)
+#    print(minimum)
+    hiba=True
+    while hiba:
+        try:
+            if tort:
+                szam=float(input(szoveg))
+            else:
+                szam=int(input(szoveg))
+        except:
+            print("valami nem jo gec")
+        else:
+            hiba=False
+szamBe("Adgyá meg numbert ppölös: ",tort=True,minimum=10)
