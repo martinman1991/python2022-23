b2cbdccf66751a444e00f0d2fddf6f4d309708d8
martinman1991 <98027494+martinman1991@users.noreply.github.com>
2022-12-16 10:35:32 +0100

    Add files via upload

--- hatalmasranovekvohonfoglalas/dogajavításh.py
+++ hatalmasranovekvohonfoglalas/dogajavításh.py
@@ -39,3 +39,4 @@ for i in range(len(jelek)):
 print(szoveg)
 
 szoveg="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet, nisl vel feugiat auctor, est nisl semper dolor, non mattis sapien ipsum eu odio. Donec fringilla lectus sed ligula sodales, vitae porttitor leo bibendum. Donec quis ligula lectus. Maecenas et ex sem. Maecenas consequat convallis lacus, quis ultricies eros dignissim sit amet. Donec ultricies est eget lacus rutrum tincidunt. Donec posuere ipsum est, vel posuere dolor consequat id. Vivamus odio ex, pharetra sit amet fringilla at, scelerisque at ex. Quisque venenatis nunc quis ex feugiat, et sodales nisl tempor. Praesent nec orci molestie, imperdiet nunc quis, rutrum sapien. Proin sodales."
+print(len(szoveg.split(" ")))
--- hatalmasranovekvohonfoglalas/szambegenerálásadvanced.py
+++ hatalmasranovekvohonfoglalas/szambegenerálásadvanced.py
@@ -28,3 +28,9 @@ szam=int(szam)
 if szam%12==0:
     print("osz6ó")
 print(szam)
+
+szoveg="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet, nisl vel feugiat auctor, est nisl semper dolor, non mattis sapien ipsum eu odio. Donec fringilla lectus sed ligula sodales, vitae porttitor leo bibendum. Donec quis ligula lectus. Maecenas et ex sem. Maecenas consequat convallis lacus, quis ultricies eros dignissim sit amet. Donec ultricies est eget lacus rutrum tincidunt. Donec posuere ipsum est, vel posuere dolor consequat id. Vivamus odio ex, pharetra sit amet fringilla at, scelerisque at ex. Quisque venenatis nunc quis ex feugiat, et sodales nisl tempor. Praesent nec orci molestie, imperdiet nunc quis, rutrum sapien. Proin sodales."
+betu="k"
+print(len(szoveg.split(" ")))
+szoveg2=szoveg.replace(betu,betu.upper())
+print(szoveg2)
